99b855956fb7a81dd001d87c08d933052594e7d9
Emna Ltaief <emnabelhajltaief@gmail.com>
2024-10-04 18:46:57 +0200

    HomePage+SignUp+LogIn+MemberProfile interfaces

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
-
+import { BrowserRouter as Router ,Route,Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './Components/HomePage/HomePage'
+import SignUp from './Components/SignUp/SignUp'
+import LogIn from './Components/LogIn/LogIn'
+import FicheMembre from './Components/FicheMembre/FicheMembre'
 
 function App() {
  
 
   return (
-    <>
-     <HomePage />
-    </>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path='/membreProfile' element={<FicheMembre />} />
+        </Routes>
+      </div>
+     
+    </Router>
   )
 }
 
---
+++ src/Components/FicheMembre/FicheMembre.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import './FicheMembre.css'
+
+const FicheMembre = () => {
+  return (
+    <>
+     <section className='section-container'>
+        <header className='title'>
+        Member Profile
+          </header>
+        <div className="container">
+        <div className="row">
+  <div className="col-md-6">
+    <div className="info-section">
+      <div className="info-item">
+        <strong>First Name:</strong> <span>John</span>
+      </div>
+      <div className="info-item">
+        <strong>Last Name:</strong> <span>Doe</span>
+      </div>
+      <div className="info-item">
+        <strong>Email:</strong> <span>john.doe@example.com</span>
+      </div>
+      <div className="info-item">
+        <strong>Phone:</strong> <span>+123456789</span>
+      </div>
+      <div className="info-item">
+        <strong>Field of Study:</strong> <span>Computer Science</span>
+      </div>
+      <div className="info-item trainings-section">
+        <strong>Acquired Trainings:</strong>
+        <div className="training-list">
+          <div className="training-item">
+            <img src="training1-logo.png" alt="Training 1 Logo" className="training-logo" />
+            <span>Training Name 1</span>
+          </div>
+          <div className="training-item">
+            <img src="training2-logo.png" alt="Training 2 Logo" className="training-logo" />
+            <span>Training Name 2</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+        </div>
+    </section> 
+    </>
+  )
+}
+
+export default FicheMembre
