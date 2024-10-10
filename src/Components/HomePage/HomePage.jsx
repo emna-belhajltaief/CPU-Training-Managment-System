8a606c7f7e6abd2b15fd989d6722dbd85f4e799d
Dhia BEN HAMOUDA <benhamoudadhia@isimm.u-monastir.tn>
2024-10-10 16:22:41 +0100

    added button to navigate to /memebrs page

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,26 +1,30 @@
 import './HomePage.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const HomePage = () => {
-  return (
- <>
-        <nav>
-            <div className="nav-logo">
-                <img src="./images/cpuwhite.png" alt="Logo"/>
+    const navigate = useNavigate();
+    return (
+        <>
+            <nav>
+                <div className="nav-logo">
+                    <img src="./images/cpuwhite.png" alt="Logo" />
+                </div>
+                <div className="nav-links">
+                    <Link to="/login">
+                        <button >LogIn</button>
+                    </Link>
+                    <Link to="/signup">
+                        <button>SignUp</button>
+                    </Link>
+                </div>
+            </nav>
+            <div className='d-flex'>
+                <h1 className='CPU_Management '>CPU Trainings Managment System </h1>
+                <button className='btn btn-secondary' onClick={() => navigate("/members")}>View members</button>
             </div>
-            <div className="nav-links">
-               <Link to="/login">
-                    <button >LogIn</button>
-                </Link>
-                <Link to="/signup">
-                    <button>SignUp</button>
-                </Link>
-            </div>
-        </nav>
-        <h1 className='CPU_Management '>CPU Trainings Managment System </h1>
         </>
 
-  )
+    )
 }
 
 export default HomePage
