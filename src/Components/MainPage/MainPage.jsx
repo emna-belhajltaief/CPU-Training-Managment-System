432d9c1104621178ebb12f4c0fb398e41c608809
Chebbi Ahmed <ssjzchebbi@gmail.com>
2024-10-13 14:48:26 +0100

    added navigation to the MainPage

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,36 +1,49 @@
+import { useNavigate } from 'react-router-dom';
 import './MainPage.css';
 import NavBar from '../NavBar/NavBar';
 
-//add code to get from the db trainings for this week
-
+// Sample formations data
 const formations = [
   {
     title: "Formation Dev : introduction HTML, CSS",
     date: "22/10/2024",
     location: "Spring Land",
     logo: "./images/HTML&CSS.png",
+    viewPath: "/formation/html-css", // Path for viewing details
+    checkInPath: "/check-in/html-css", // Path for check-in
   },
   {
     title: "Formation Robotic : introduction stem32",
     date: "29/10/2024",
     location: "ISIMM-C11",
     logo: "./images/STM32.png",
+    viewPath: "/formation/stem32", // Path for viewing details
+    checkInPath: "/check-in/stem32", // Path for check-in
   },
   {
     title: "Formation Dev : C++ for beginners",
     date: "9/11/2024",
     location: "Les Jumelles",
     logo: "./images/C++_logo.png",
+    viewPath: "/formation/cpp", // Path for viewing details
+    checkInPath: "/check-in/cpp", // Path for check-in
   },
 ];
 
 const MainPage = () => {
-  
+  const navigate = useNavigate(); // Initialize the navigate function
+
+  const handleView = () => {
+    navigate('/FormulaireFormation'); // Navigate to the specified path
+  };
+
+  const handleCheckIn = () => {
+    navigate("/CheckIn"); // Navigate to the check-in path
+  };
 
   return (
     <>
-
-    <NavBar></NavBar>
+      <NavBar />
       <main className="formation-list">
         {formations.map((formation, index) => (
           <div key={index} className="formation-item">
@@ -40,10 +53,19 @@ const MainPage = () => {
               <p>Date: {formation.date}</p>
               <p>Location: {formation.location}</p>
             </div>
-            
             <div className="edit_button">
-            <button className="edit-btn">View</button>
-            <button className="edit-btn">Open for check-in</button>
+              <button 
+                className="edit-btn" 
+                onClick={() => handleView()}
+              >
+                View
+              </button>
+              <button 
+                className="edit-btn" 
+                onClick={() => handleCheckIn()}
+              >
+                Open for check-in
+              </button>
             </div>
           </div>
         ))}
